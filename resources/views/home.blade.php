ba46021db05b65c457d937ef818f83b54104bb82
BuildTools <unconfigured@null.spigotmc.org>
2024-11-01 16:17:39 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1 class="text-center">Selamat datang di home page</h1>
+    <p class="text-muted">Ini adalah halaman utama yang dibuat dengan Laravel Blade dan Bootstrap</p>
+    <a href="" class="btn btn-primary">Pelajari lebih lanjut</a>
+@endsection
